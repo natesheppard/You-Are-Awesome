3b2dd803391486887f6f0bf95fc6d6c54ecb4588
Nate Sheppard <nsheppard@zagmail.gonzaga.edu>
2020-09-02 22:11:26 -0700

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Nate on 9/2/20.
+//  Copyright © 2020 Nate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
